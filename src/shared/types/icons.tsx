c98903df947b41d15cde10cc19c3e39886e4f10a
user <boyarkina.ar@gmail.com>
2026-01-11 22:29:06 +0200

    api, icons

---
+++ src/shared/types/icons.tsx
@@ -0,0 +1,58 @@
+import ArrowForward from "../../assets/icons/arrow-forward.png"
+import Avatar from "../../assets/icons/avatar.png"
+import Calendar from "../../assets/icons/calendar.png"
+import Cart from "../../assets/icons/cart.png"
+import Copy from "../../assets/icons/copy.png"
+import DownOutline from "../../assets/icons/down-outline.png"
+import Edit from "../../assets/icons/edit.png"
+import EyeOff from "../../assets/icons/eye-off.png"
+import EyeOn from "../../assets/icons/eye-on.png"
+import Facebook from "../../assets/icons/facebook.png"
+import FooterText from "../../assets/icons/footer-text.png"
+import Frame from "../../assets/icons/frame.png"
+import Hamburger from "../../assets/icons/hamburger.png"
+import Instagram from "../../assets/icons/instagram.png"
+import LightAvatar from "../../assets/icons/light-avatar.png"
+import LightCart from "../../assets/icons/light-cart.png"
+import Mail from "../../assets/icons/mail.png"
+import Map from "../../assets/icons/map.png"
+import Minus from "../../assets/icons/minus.png"
+import Phone from "../../assets/icons/phone.png"
+import PinMap from "../../assets/icons/pin-map.png"
+import Plus from "../../assets/icons/plus.png"
+import Search from "../../assets/icons/search.png"
+import Spinner from "../../assets/icons/spinner.png"
+import Telegram from "../../assets/icons/telegram.png"
+import Trash from "../../assets/icons/trash.png"
+import Warning from "../../assets/icons/warning.png"
+
+
+export const icons = {
+    ArrowForward,
+    Avatar,
+    Calendar,
+    Cart,
+    Copy,
+    DownOutline,
+    Edit,
+    EyeOff,
+    EyeOn,
+    Facebook,
+    FooterText,
+    Frame,
+    Hamburger,
+    Instagram,
+    LightAvatar,
+    LightCart,
+    Mail,
+    Map,
+    Minus,
+    Phone,
+    PinMap,
+    Plus,
+    Search,
+    Spinner,
+    Telegram,
+    Trash,
+    Warning
+}
