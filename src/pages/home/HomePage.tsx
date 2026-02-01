815155ac67759bdb679b676c4fc8aff79650be5f
user <boyarkina.ar@gmail.com>
2026-02-01 14:59:21 +0200

    this commit doesn't work

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -5,11 +5,9 @@ import styles from "./home.module.css";
 
 
 export function HomePage(){
-    return (
-        <div className={styles.app}>
+    return <div className={styles.app}>
             <Header />
             <Main />
             <Footer />
         </div>
-    )
 }
