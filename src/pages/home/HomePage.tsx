5fc0ffab884d4ddade8dc7dca84709e110f54276
user <boyarkina.ar@gmail.com>
2026-02-01 15:13:49 +0200

    early merge

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,13 +1,10 @@
 import { Footer } from "../../app/footer/Footer";
 import { Header, HeaderSimple } from "../../app/header/Header";
+import { Layout } from "../../app/layout/Layout";
 import { Main } from "../../app/main/Main";
 import styles from "./home.module.css";
 
 
 export function HomePage(){
-    return (
-        <div className={styles.app}>
-            <Main />
-        </div>
-    )
+    return <Layout />
 }
